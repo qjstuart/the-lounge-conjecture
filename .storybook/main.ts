9e7855abc585181d59c0cf59d1c3b9ae345fc281
qjstuart <quentinqjs@gmail.com>
2025-10-28 12:15:26 +0100

    add import alias to storybook vite config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,29 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
+import path from "path";
 
 const config: StorybookConfig = {
-  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
   ],
   framework: {
     name: "@storybook/nextjs-vite",
     options: {},
   },
   staticDirs: ["../public"],
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  async viteFinal(config) {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...(config.resolve?.alias || {}),
+          "@": path.resolve(__dirname, "../"),
+        },
+      },
+    };
+  },
 };
 export default config;
